a03aa58b47a2916ba3ab92a99ab8b070479867cd
joeylmaalouf <joeylmaalouf@gmail.com>
2016-03-10 16:20:19 -0500

    tweak

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
   github: {
     clientID: "1600bfa822f2f87ac496",
     clientSecret: "9535812516537865d739374948f5c9c53635c518",
-    callbackURL: "http://cloud-tube.herokuapp.com/auth/github/callback"
+    callbackURL: "http://localhost:3000/auth/github/callback"
   }
 };
 
